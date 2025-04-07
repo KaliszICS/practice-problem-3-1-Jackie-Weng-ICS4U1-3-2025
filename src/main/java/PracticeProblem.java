f081c69aaede15ce7dc85ce789799eaba2f684e8
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-07 21:56:21 +0000

    COMPLETED

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,33 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String[] args) {
+        System.out.println("sumOfDigits(234): " + sumOfDigits(234));    
+        System.out.println("sumOfDigits(12): " + sumOfDigits(12));      
+        System.out.println("sumOfDigits(-12): " + sumOfDigits(-12));    
+        System.out.println("sumOfDigits(39): " + sumOfDigits(39));      
 
-	}
+        System.out.println("factorial(5): " + factorial(5));           
+        System.out.println("factorial(0): " + factorial(0));           
+        System.out.println("factorial(-3): " + factorial(-3));         
+    }
 
-	
+    public static int sumOfDigits(int x) {
+        if (x < 0) {
+            x = -x;
+        }
+        if (x < 10) {
+            return x;
+        }
+        return (x % 10) + sumOfDigits(x / 10);
+    }
 
+    public static int factorial(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
 }
