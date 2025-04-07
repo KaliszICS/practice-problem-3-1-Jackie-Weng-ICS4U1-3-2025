490a710123a1014a4abc829fe99b9ba76f98bfff
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-07 21:57:11 +0000

    COMPLETED 2 (i dont need the prints)

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,16 +1,5 @@
 public class PracticeProblem {
 
-    public static void main(String[] args) {
-        System.out.println("sumOfDigits(234): " + sumOfDigits(234));    
-        System.out.println("sumOfDigits(12): " + sumOfDigits(12));      
-        System.out.println("sumOfDigits(-12): " + sumOfDigits(-12));    
-        System.out.println("sumOfDigits(39): " + sumOfDigits(39));      
-
-        System.out.println("factorial(5): " + factorial(5));           
-        System.out.println("factorial(0): " + factorial(0));           
-        System.out.println("factorial(-3): " + factorial(-3));         
-    }
-
     public static int sumOfDigits(int x) {
         if (x < 0) {
             x = -x;
